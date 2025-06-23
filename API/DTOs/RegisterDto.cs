b8831e069e2301c700bbdf10812975dab795e2ab
LEONIDAS <LEONIDAS@DESKTOP-AURMCCU>
2025-06-23 18:19:09 +0300

    Implemented Registration & Login

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    [MaxLength(20)]
+    public required string Username { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
