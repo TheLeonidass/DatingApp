676c5761b22851be586e646dc030e338a0ea9029
LEONIDAS <gleonidas2000@gmail.com>
2025-07-27 19:22:27 +0300

    Added Messaging Functionality

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,7 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -19,5 +20,13 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 
         builder.Entity<UserLike>().HasOne(s => s.TargetUser).WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId).OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(x => x.MessagesReceived).OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent).OnDelete(DeleteBehavior.Restrict);
     }
 }
